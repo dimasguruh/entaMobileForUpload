9742d80f23f3f81fd7e1b764963071493b18411d
Guruh Sarwono <guruh.sarwono@gmail.com>
2024-05-14 15:27:47 +0700

    Add project files.

---
+++ lib/args/general.dart
@@ -0,0 +1,19 @@
+import 'package:enta_mobile/models/clocking.dart';
+import 'package:flutter/material.dart';
+
+class GeneralArgs {
+  Key key;
+  String alertText, url, title, type;
+  bool showAlert;
+  ClockingHistoryModel clocking;
+
+  GeneralArgs({
+    this.key,
+    this.showAlert,
+    this.alertText,
+    this.url,
+    this.title,
+    this.clocking,
+    this.type,
+  });
+}
